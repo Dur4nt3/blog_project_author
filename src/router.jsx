64e8bc9acbbcfcbed98366b4e498a6a278bc864a
Dur4nt3 <189136258+Dur4nt3@users.noreply.github.com>
2026-02-03 16:01:51 +0200

    Initial setup

--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,22 @@
 import { createBrowserRouter } from 'react-router';
 
-import App from './App.jsx';
+import Root from './modules/routes/root/Root';
+import Login from './modules/routes/root/Login';
+import Error404 from './modules/routes/errors/Error404';
+
+import rootLoader from './modules/utilities/loaders/rootLoader';
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <App />,
+        element: <Root />,
+        errorElement: <Error404 />,
+        loader: rootLoader,
+    },
+    {
+        path: 'login',
+        element: <Login />,
+        errorElement: <Error404 />,
     },
 ]);
 
